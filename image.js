6e70b54f2b8eafb931f275615893dfc9802d0e89
TipsyDr <tispydr@163.com>
2022-02-14 01:34:30 +0800

    加入视频模块

--- image.js
+++ image.js
@@ -1,11 +1,13 @@
 "use strict";
 
 const fs = require("fs");
-const path = "./image";
-
-function getMdFiles(name, urls) {
+const getPicPath = "./image";
+const getMoviePath = "./movie";
+const picPath = '../source/image';
+const moviePath = '../source/movie';
+function creatPicMd(name, urls) {
   const temp =
-    `
+`
 ---
 title: ${name}
 ---
@@ -14,7 +16,7 @@ title: ${name}
 ${urls}
 {% endgallery %}
 
-`
+`;
   return temp;
 }
 
@@ -31,6 +33,25 @@ ${data}
   return temp;
 }
 
+function creatMovieMd(name, data) {
+  const temp = 
+`
+---
+title: ${name}
+---
+
+${data}
+
+`;
+
+  return temp;
+}
+
+
+
+
+
+
 function getFilesName(path) {
   return new Promise(function (resolve, reject) {
     fs.readdir(path, function (err, res) {
@@ -39,34 +60,41 @@ function getFilesName(path) {
   })
 }
 
-async function asyncGetFilesName(path) {
+async function asyncGetFilesName(path, toPath, type, func) {
   let files1 = await getFilesName(path);
   let indexData = '';
-
+  const remotPath = 'https://blog-image-rosy.vercel.app';
   for (let i = 0; i < files1.length; i++) {
     const name = files1[i];
     const data = await getFilesName(path + '/' + name);
     let urls = '';
     let cover = '';
     for (let j = 0; j < data.length; j++) {
-      const item = `![](https://blog-image-rosy.vercel.app/image/${name}/${data[j]})`;
-      urls += item;
+      const url = `${remotPath}/${type}/${name}/${data[j]}`;
+      const item = type === 'image' ? 
+        `![](${url})`
+        : 
+        `<video src="${url}" width="350px" height="200px" controls="controls"></video>`;
       if (item.indexOf('cover') > 0) {
-        cover = data[i];
+        cover = data[j];
       }
+      if(type !== 'movie' || url.indexOf('cover') < 0) {
+        urls += item;
+      };
     }
-    const coverItem = `{% galleryGroup ${name} 关于${name}的图片 /gallery/${name} https://blog-image-rosy.vercel.app/image/${name}/${cover} %}`;
+    const typeText = type === 'image' ? '图片' : '电影';
+    const coverItem = `{% galleryGroup ${name} 关于${name}的${typeText} /${type}/${name} ${remotPath}/${type}/${name}/${cover} %}`;
     indexData += coverItem;
-    const mdPath = `../source/gallery/${name}`
+    const mdPath = `${toPath}/${name}`
     if (!fs.existsSync(mdPath)) {
       fs.mkdirSync(mdPath);
     }
-    fs.writeFile(`${mdPath}/index.md`, getMdFiles(name, urls), (e) => console.log('success'));
+    fs.writeFile(`${mdPath}/index.md`, func(name, urls), (e) => console.log('success'));
   }
-  fs.writeFile(`../source/gallery/index.md`, getIndexMdFiles(indexData), (e) => console.log('success'));
+  fs.writeFile(`${toPath}/index.md`, getIndexMdFiles(indexData), (e) => console.log('success'));
 }
 
 
 
-asyncGetFilesName(path);
-
+asyncGetFilesName(getPicPath, picPath, 'image', creatPicMd);
+asyncGetFilesName(getMoviePath, moviePath, 'movie', creatMovieMd);
